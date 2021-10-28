f3ec426314c3d548f73020cb64bfc739d0efea1f
Alejandro Leal <aleaberm12@gmail.com>
2021-10-28 23:11:03 +0200

    dependencias actualizadas

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
-import { StatusBar, View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,15 +9,11 @@ import SplashScreen from 'react-native-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Tutorial from './src/screens/Tutorial';
 import Home from './src/screens/Home';
-import Settings from './src/screens/Settings';
 
 const store = configureStore();
-const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -34,41 +30,17 @@ const App = () => {
 		NetInfo.addEventListener((state) => setIsConnected(state.isConnected));
 	};
 
-	const drawer = () => {
-		return (
-			<Drawer.Navigator initialRouteName="Home">
-				<Drawer.Screen
-					name="Home"
-					component={Home}
-					options={{ headerShown: false }}
-				/>
-				<Drawer.Screen
-					name="Settings"
-					component={Settings}
-					options={{ headerShown: false }}
-				/>
-			</Drawer.Navigator>
-		);
-	};
-
 	return (
 		<SafeAreaProvider>
 			<Provider store={store.store}>
 				<PersistGate loading={null} persistor={store.persistor}>
-					<StatusBar backgroundColor="#1775C2" />
-
 					<NavigationContainer>
-						<Stack.Navigator initialRouteName="Tutorial">
-							<Stack.Screen
-								name="Tutorial"
-								component={Tutorial}
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="Home"
-								component={drawer}
-								options={{ headerShown: false }}
-							/>
+						<Stack.Navigator 
+							screenOptions={{ 
+								headerShown: false,
+							}}
+						>
+							<Stack.Screen name="Home" component={Home} />
 						</Stack.Navigator>
 					</NavigationContainer>
 
